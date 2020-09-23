48219900bd83926d255403aa589713999631b822
unknown <ncuti60@gmail.com>
2020-09-23 13:21:18 +0200

    ch(setup):set up sequelize
    ch(setup):generate config and model
    
    ch:update gitignore
    
    ch(package):add packages
    
    ch(package):add migrate and seeding in scripts
    
    ch(database):setting sequelize
    
    ch(package):add packages
    
    ft(package):add migrate reset
    
    ch:added port in detenv example

--- src/routes/routeWelcome.js
+++ src/routes/routeWelcome.js
@@ -3,16 +3,6 @@ import express from "express";
 
 const router = express.Router();
 
-/**
- * @swagger
- * /:
- *  get:
- *    description: Use to request all customers
- *    responses:
- *      '200':
- *        description: A successful response
- */
-
 router.route("/").get(welcomeController.getWelcome)
 
 export default router
