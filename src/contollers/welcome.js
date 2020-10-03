38213aa9b401a113cc56244907411ec838d72989
unknown <ncuti60@gmail.com>
2020-10-03 20:58:30 +0200

    ft(locales):add languages
    ft(src):update src/
    
    ft(package):update packages
    
    ft(readme):add test coverage
    
    ft(eslint):update eslint
    
    ft(travis):update travis file
    
    ft:add new line

--- src/contollers/welcome.js
+++
@@ -1,9 +0,0 @@
-import app from '../languages/international';
-exports.getWelcome = app.get('/',(req, res) =>
- {
-    res.status(200).send({
-        status:res.__("ok"),
-        message:res.__('Welcome')
-    })
-  });
-
