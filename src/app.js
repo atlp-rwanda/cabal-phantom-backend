9f2475a9a4ea300d180d14a4b0c7b5e281da89a3
unknown <ncuti60@gmail.com>
2020-10-03 20:58:30 +0200

    ft(locales):add languages
    ft(src):update src/
    
    ft(package):update packages
    
    ft(readme):add test coverage
    
    ft(eslint):update eslint
    
    ft(travis):update travis file
    
    ft:add new line

--- src/app.js
+++ src/app.js
@@ -1,15 +1,37 @@
-
 import express from "express";
-import welcomeRoute from "./routes/routeWelcome"
+import i18n from 'i18n'
+import bodyParser from 'body-parser'
 import swaggerUiExpress from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 
-
 const app = express();
+app.use(bodyParser.json());
 
+i18n.configure({
+    locales: ['en', 'fr', 'rw'],
+    header: 'accept-language',
+    extension: '.json',
+    queryParameter: 'lang',
+    directoryPermissions: '755',
+    autoReload: true,
+    directory: __dirname + '/../locales'
+});
+
+app.use(i18n.init)
 app.use('/phantom-docs', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocument));
 
-app.use("/",welcomeRoute);
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: res.__('welcome')
+    })
+})
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        message: res.__("404")
+    })
+})
 
 export default app;
+
 
--- src/test/test.js
+++ src/test/test.js
@@ -5,16 +5,26 @@ import chaiHttp from "chai-http";
 import { expect } from "chai"
 chai.use(chaiHttp);
 
-describe("Phantom testing", () => {
-  it("Phantom", done => {
-    chai
-      .request(app)
-      .get("/")
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        
-          expect(res.body.message).to.equals('Welcome to our phantom beginning');
+describe("Phantom API test", () => {
+  describe('/GET Welcome', () => {
+    it('it should GET welcome', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
           done();
         });
     });
+  });
+
+  describe('/all unspecified routes', () => {
+    it('it should handle all unspecified routes', (done) => {
+      chai.request(app)
+        .get('/ghyy')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(404);
+          done();
+        });
+    });
+  });
 });
