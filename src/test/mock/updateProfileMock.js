b651cee3a8543efe87b48ae146b398f97e7a0007
Niyiragira Gerard <niyiragiragerard@gmail.com>
2020-10-30 08:08:01 +0200

    ft-updates-profile
    icreasing coverage
    
    ft-backend-update-profile

---
+++ src/test/mock/updateProfileMock.js
@@ -0,0 +1,3 @@
+exports.token = {
+    oparator:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjA0Mzk3MTU5LCJleHAiOjE2MDQ0ODM1NTl9.7-y_FsyFqs1T6r2XUIO-vtv_sVPlJJP505nk7K-kT8w"
+}
---
+++ src/test/testProfileUpdate.js
@@ -0,0 +1,34 @@
+import chai, { expect } from 'chai';
+import chaiHttp from "chai-http";
+import { describe, it } from 'mocha';
+import app from "../app";
+import registerMock from './mock/registerMocks'
+import updateMock from '../test/mock/updateProfileMock'
+
+chai.use(chaiHttp);
+
+describe("Phantom API test update profile", () => {
+  it("Should not update user if name is empty ", done=>{
+    chai 
+      .request(app)
+      .patch("/api/v1/auth/updateProfile")
+      .set("Authorization", updateMock.token.oparator)
+      .send(registerMock.nameIsEmpty)
+      .end((err,res)=>{
+        expect(res).to.have.status(400);
+        done();
+      })
+  }); 
+  it("Should not update user if name is empty ", done=>{
+    chai 
+      .request(app)
+      .patch("/api/v1/auth/updateProfile")
+      .set("Authorization", updateMock.token.oparator)
+      .send(registerMock.invalidEmail)
+      .end((err,res)=>{
+        expect(res).to.have.status(401);
+        expect(res.body.message).to.equal("You have to use a registered email in order to update your profile")
+        done();
+      })
+  }); 
+  });
